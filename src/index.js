2733721dbbc6f71a8fb018255ab423579f29a8ea
Mariah Morales <mjustine622@gmail.com>
2023-05-22 14:09:48 -0400

    Adding .gitignore file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const key = config.my_key
+
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    // Accessing Geolocation of User
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+         
+      });
+    }
+  });
+
